347b9690b7f288231a065409d7d388481dba6038
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-05 19:26:01 +0000

    automatically backed up by learn

---
+++ lib/author.rb
@@ -0,0 +1,14 @@
+class Author
+  attr_accessor :name, :posts
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    self.posts << post
+    post.author = self
+  end
+  
+end
