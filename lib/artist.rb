dcace59f6b3061d95a36667e383d9a5dd9616212
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-05 18:11:35 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,8 +9,8 @@ class Artist
     @songs = []
   end
 
-  def add_song(song_name)
-    song = Song.new(song_name)
+  def add_song()
+  
     @songs << song
     song.artist = self
     # @@song_count += 1
