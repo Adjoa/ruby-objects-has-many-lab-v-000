a40934676e37a1000d66ffa194db93ed4fe82d7f
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-05 19:11:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,11 +7,7 @@ class Song
   end
 
   def artist_name
-    if defined? self.artist
-      self.artist.name
-    else
-      nil
-    end
+    self.artist.name unless self.artist == nil
   end
 
 end
