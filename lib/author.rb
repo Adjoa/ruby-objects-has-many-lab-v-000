8747727e1d90dbdc988fbfe1c87984b42383073c
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-05 19:32:02 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,16 +11,18 @@ class Author
   def add_post(post)
     self.posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
     self.posts << post
+    @@post_count += 1
   end
 
   def self.post_count
     @@post_count
   end
-  
+
 end
